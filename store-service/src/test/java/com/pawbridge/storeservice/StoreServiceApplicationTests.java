74548a705d7f917e17de9334a1a830211815bc2b
유성호 <shyu6370@gmail.com>
2026-08-28 17:52:03 +0900

    fix: R2 업로드 설정 범위 정리

--- store-service/src/test/java/com/pawbridge/storeservice/StoreServiceApplicationTests.java
+++ store-service/src/test/java/com/pawbridge/storeservice/StoreServiceApplicationTests.java
@@ -3,7 +3,14 @@ package com.pawbridge.storeservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.cloud.aws.credentials.access-key=test-access-key",
+		"spring.cloud.aws.credentials.secret-key=test-secret-key",
+		"spring.cloud.aws.region.static=auto",
+		"spring.cloud.aws.s3.endpoint=http://localhost:9000",
+		"spring.cloud.aws.s3.bucket=test-bucket",
+		"pawbridge.storage.public-base-url=http://localhost:9000/test-bucket"
+})
 class StoreServiceApplicationTests {
 
 	@Test
